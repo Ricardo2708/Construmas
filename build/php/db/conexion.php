0d5abde4ed3ee3015d191acfad4022832f3fc20f
Ricardo2708 <78193375+Ricardo2708@users.noreply.github.com>
2023-02-15 15:08:18 -0600

    Update conexion.php

--- build/php/db/conexion.php
+++ build/php/db/conexion.php
@@ -3,9 +3,9 @@
 try {
   
   function insertar(string $nombre, string $email, string $telefono){
-    $user = "root";
-    $password = "Rikyjo11*";
-    $database = "construmas";
+    $user = "construmascom_ricardo";
+    $password = "rikyjo11";
+    $database = "construmascom_web";
  
     $conn = mysqli_connect("127.0.0.1", "{$user}", "{$password}", "{$database}");
     $sql = "INSERT INTO suscripciones (nombre,email,telefono) VALUES ('{$nombre}', '{$email}', '{$telefono}' )";
